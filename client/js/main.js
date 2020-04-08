723dd0f1274f73ec1e4c893090ad60ab751ceb7c
fuza-ail <mfuzail1991@gmail.com>
2020-04-08 12:13:29 +0700

    vue

---
+++ client/js/main.js
@@ -0,0 +1,11 @@
+var app = new  Vue({
+  el: '#app',
+  data:{
+    categories:{
+      Backlog : ['backlog 1', 'backlog 2'],
+      Todo : ['todo 1', 'todo 2','todo 3'],
+      Done : ['done 1','done 2', 'done 3', 'done 4'],
+      Completed: ['completed 1']
+    }
+  }
+})
